2503949b95add574cd1281542d925d2d2c458a87
Mick Staugaard <spam@staugaard.com>
2008-12-05 20:29:22 +0100

    added methods for canceling recurring payments to the PayPal Express Gateway

--- lib/active_merchant/billing/gateways/paypal_express.rb
+++ lib/active_merchant/billing/gateways/paypal_express.rb
@@ -28,7 +28,7 @@ module ActiveMerchant #:nodoc:
       def setup_recurring(options = {})
         requires!(options, :return_url, :cancel_return_url, :description)
         
-        commit 'SetCustomerBillingAgreement', build_setup_recurring_request(options)
+        commit 'SetCustomerBillingAgreement', build_setup_recurring_payment_request(options)
       end
 
       def details_for(token)
@@ -54,11 +54,15 @@ module ActiveMerchant #:nodoc:
       def recurring(money, options = {})
         requires!(options, :token, :period)
 
-        commit 'CreateRecurringPaymentsProfile', build_create_recurring_profile_request(money, options)
+        commit 'CreateRecurringPaymentsProfile', build_create_recurring_payment_profile_request(money, options)
       end
       
-      def get_profile_details(profile_id)
-        commit 'GetRecurringPaymentsProfileDetails', build_get_profile_details_request(profile_id)
+      def recurring_inquiry(profile_id, options = {})
+        commit 'GetRecurringPaymentsProfileDetails', build_get_recurring_payment_profile_details_request(profile_id)
+      end   
+
+      def cancel_recurring(profile_id, options = {})
+        commit 'ManageRecurringPaymentsProfileStatus', build_manage_recurring_payment_profile_status_request(profile_id, :cancel, options)
       end
 
       private
@@ -118,50 +122,6 @@ module ActiveMerchant #:nodoc:
         xml.target!
       end
       
-      def build_create_recurring_profile_request(money, options)
-        currency_code = options[:currency] || currency(money)
-
-        xml = Builder::XmlMarkup.new :indent => 2
-        xml.tag! 'CreateRecurringPaymentsProfileReq', 'xmlns' => PAYPAL_NAMESPACE do
-          xml.tag! 'CreateRecurringPaymentsProfileRequest', 'xmlns:n2' => EBAY_NAMESPACE do
-            xml.tag! 'n2:Version', API_VERSION
-            xml.tag! 'n2:CreateRecurringPaymentsProfileRequestDetails' do
-              xml.tag! 'n2:Token', options[:token]
-              
-              xml.tag! 'n2:RecurringPaymentsProfileDetails' do
-                xml.tag! 'n2:SubscriberName', options[:subscriber_name] unless options[:subscriber_name].blank?
-                xml.tag! 'n2:BillingStartDate', (options[:start_date] || Time.now).utc.iso8601
-                xml.tag! 'n2:ProfileReference', options[:reference] unless options[:reference].blank?
-              end
-              
-              xml.tag! 'n2:ScheduleDetails' do
-                xml.tag! 'n2:Description', options[:description]
-                xml.tag! 'n2:PaymentPeriod' do
-                  xml.tag! 'n2:BillingPeriod', options[:period].to_s.camelize
-                  xml.tag! 'n2:BillingFrequency', options[:frequency].blank? ? 1 : options[:frequency].to_i
-                  xml.tag! 'n2:Amount', amount(money), 'currencyID' => currency_code
-                end
-                xml.tag! 'n2:MaxFailedPayments', options[:max_failed_payments] unless options[:max_failed_payments].blank?
-              end
-            end
-          end
-        end
-
-        xml.target!
-      end
-
-      def build_get_profile_details_request(profile_id)
-        xml = Builder::XmlMarkup.new :indent => 2
-        xml.tag! 'GetRecurringPaymentsProfileDetailsReq', 'xmlns' => PAYPAL_NAMESPACE do
-          xml.tag! 'GetRecurringPaymentsProfileDetailsRequest', 'xmlns:n2' => EBAY_NAMESPACE do
-            xml.tag! 'n2:Version', API_VERSION
-            xml.tag! 'ProfileID', profile_id
-          end
-        end
-      
-        xml.target!
-      end
-            
       def build_setup_request(action, money, options)
         xml = Builder::XmlMarkup.new :indent => 2
         xml.tag! 'SetExpressCheckoutReq', 'xmlns' => PAYPAL_NAMESPACE do
@@ -198,7 +158,9 @@ module ActiveMerchant #:nodoc:
         xml.target!
       end
       
-      def build_setup_recurring_request(options)
+      #RECURRING PAYMENTS REQUESTS
+      
+      def build_setup_recurring_payment_request(options)
         xml = Builder::XmlMarkup.new :indent => 2
         xml.tag! 'SetCustomerBillingAgreementReq', 'xmlns' => PAYPAL_NAMESPACE do
           xml.tag! 'SetCustomerBillingAgreementRequest', 'xmlns:n2' => EBAY_NAMESPACE do
@@ -220,6 +182,83 @@ module ActiveMerchant #:nodoc:
               xml.tag! 'n2:cpp-header-border-color', options[:header_border_color] unless options[:header_border_color].blank?
               xml.tag! 'n2:cpp-payflow-color', options[:background_color] unless options[:background_color].blank?
               
+            end
+          end
+        end
+
+        xml.target!
+      end
+      
+      def build_create_recurring_payment_profile_request(money, options)
+        currency_code = options[:currency] || currency(money)
+
+        xml = Builder::XmlMarkup.new :indent => 2
+        xml.tag! 'CreateRecurringPaymentsProfileReq', 'xmlns' => PAYPAL_NAMESPACE do
+          xml.tag! 'CreateRecurringPaymentsProfileRequest', 'xmlns:n2' => EBAY_NAMESPACE do
+            xml.tag! 'n2:Version', API_VERSION
+            xml.tag! 'n2:CreateRecurringPaymentsProfileRequestDetails' do
+              xml.tag! 'n2:Token', options[:token]
+              
+              xml.tag! 'n2:RecurringPaymentsProfileDetails' do
+                xml.tag! 'n2:SubscriberName', options[:subscriber_name] unless options[:subscriber_name].blank?
+                xml.tag! 'n2:BillingStartDate', (options[:start_date] || Time.now).utc.iso8601
+                xml.tag! 'n2:ProfileReference', options[:reference] unless options[:reference].blank?
+              end
+              
+              xml.tag! 'n2:ScheduleDetails' do
+                xml.tag! 'n2:Description', options[:description]
+                xml.tag! 'n2:PaymentPeriod' do
+                  xml.tag! 'n2:BillingPeriod', options[:period].to_s.camelize
+                  xml.tag! 'n2:BillingFrequency', options[:frequency].blank? ? 1 : options[:frequency].to_i
+                  xml.tag! 'n2:Amount', amount(money), 'currencyID' => currency_code
+                end
+                xml.tag! 'n2:MaxFailedPayments', options[:max_failed_payments] unless options[:max_failed_payments].blank?
+              end
+            end
+          end
+        end
+
+        xml.target!
+      end
+
+      def build_get_recurring_payment_profile_details_request(profile_id)
+        xml = Builder::XmlMarkup.new :indent => 2
+        xml.tag! 'GetRecurringPaymentsProfileDetailsReq', 'xmlns' => PAYPAL_NAMESPACE do
+          xml.tag! 'GetRecurringPaymentsProfileDetailsRequest', 'xmlns:n2' => EBAY_NAMESPACE do
+            xml.tag! 'n2:Version', API_VERSION
+            xml.tag! 'ProfileID', profile_id
+          end
+        end
+      
+        xml.target!
+      end
+      
+      def build_manage_recurring_payment_profile_status_request(profile_id, action, options)
+        xml = Builder::XmlMarkup.new :indent => 2
+        xml.tag! 'ManageRecurringPaymentsProfileStatusReq', 'xmlns' => PAYPAL_NAMESPACE do
+          xml.tag! 'ManageRecurringPaymentsProfileStatusRequest', 'xmlns:n2' => EBAY_NAMESPACE do
+            xml.tag! 'n2:Version', API_VERSION
+            xml.tag! 'n2:ManageRecurringPaymentsProfileStatusRequestDetails' do
+              xml.tag! 'n2:ProfileID', profile_id
+              xml.tag! 'n2:Action', action.to_s.camelize
+              xml.tag! 'n2:Note', options[:note].strip unless options[:note].blank?
+            end
+          end
+        end
+
+        xml.target!
+      end
+      
+      def build_bill_outstanding_amount_request(profile_id, money = nil)
+        currency_code = options[:currency] || currency(money)
+
+        xml = Builder::XmlMarkup.new :indent => 2
+        xml.tag! 'BillOutstandingAmountReq', 'xmlns' => PAYPAL_NAMESPACE do
+          xml.tag! 'BillOutstandingAmountRequest', 'xmlns:n2' => EBAY_NAMESPACE do
+            xml.tag! 'n2:Version', API_VERSION
+            xml.tag! 'n2:BillOutstandingAmountRequestDetails' do
+              xml.tag! 'n2:ProfileID', profile_id
+              xml.tag! 'n2:Amount', amount(money), 'currencyID' => currency_code unless money.nil?
             end
           end
         end
